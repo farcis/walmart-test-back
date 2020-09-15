3b13b9532e2b0206025982cc59ecedcabc2aa73d
Francisco <francisco.arcis.s@gmail.com>
2020-09-15 15:01:37 -0300

    Basic Springboot files for backend app

---
+++ src/main/java/com/walmart/walmarttestback/WalmartTestBackApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.walmarttestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalmartTestBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalmartTestBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/walmarttestback/WalmartTestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.walmarttestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalmartTestBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
